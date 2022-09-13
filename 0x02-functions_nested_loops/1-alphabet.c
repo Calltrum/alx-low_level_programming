ba69a9875128be9ba79a82264b7554a76a719b3c
Chidera Omeje <Chideraomeje190@gmail.com>
2022-09-13 15:04:15 -0700

    1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Prints the alphabet, in lowercase
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+
+	_putchar('\n');
+	return (0);
+}
