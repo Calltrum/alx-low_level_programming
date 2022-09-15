7a7da5974c1533d910da3a0a6962820f7774c89b
Chidera Omeje <Chideraomeje190@gmail.com>
2022-09-15 05:36:45 -0700

    0. isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - check for uppercase character.
+ * _isupper - check for uppercase character.
  * @c: the character to be checked
  * Return: 1 if c is uppercase, 0 otherwise
  */
