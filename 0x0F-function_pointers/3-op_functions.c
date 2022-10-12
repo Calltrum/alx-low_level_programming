612ab969f5be8201de0debe50b639126f9dcc773
Chidera Omeje <Chideraomeje190@gmail.com>
2022-10-12 16:54:31 -0700

    3. A goal is not always meant to be reached, it often serves simply as something to aim at

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,54 +1,48 @@
-#include "3-calc.h"
-
 /**
- * op_add - addition operator
- * @a: first int
- * @b: second int
- * Return: sum of 2 numbers
+ * op_add - sum of a and b
+ * @a: first operand
+ * @b: second operand
+ * Return: a + b
  */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
-
 /**
- * op_sub - subtraction operator
- * @a: first in
- * @b: second int
- * Return: difference of 2 numbers
+ * op_sub - substract of a and b
+ * @a: first operand
+ * @b: second operand
+ * Return: a - b
  */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
-
 /**
- * op_mul - product of 2 numbers
- * @a: first int
- * @b: second int
- * Return: product of the numbers
+ * op_mul - multiply of a and b
+ * @a: first operand
+ * @b: second operand
+ * Return: a * b
  */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
-
 /**
- * op_div - division of 2 numbers
- * @a: first int
- * @b: second int
- * Return: result of the division of the numbers
+ * op_div - divide of a and b
+ * @a: first operand
+ * @b: second operand
+ * Return: a / b
  */
 int op_div(int a, int b)
 {
 	return (a / b);
 }
-
 /**
- * op_mod - get remainder of the division of 2 numbers
- * @a: first int
- * @b: second int
- * Return: remainder of division of the numbers
+ * op_mod - module of a and b
+ * @a: first operand
+ * @b: second operand
+ * Return: a % b
  */
 int op_mod(int a, int b)
 {
